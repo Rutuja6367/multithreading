44e727e15f481fe5e9845ff785287efa56e3c7d0
Rutuja Patil <rutujapatil@Rutujas-MacBook-Air.local>
2025-07-26 17:57:54 -0400

    multi-tasking

---
+++ TransactionMultithreading/src/main/java/com/example/transactionmultithreading/TransactionMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionmultithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMultithreadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionMultithreadingApplication.class, args);
+    }
+
+}
---
+++ TransactionMultithreading/src/test/java/com/example/transactionmultithreading/TransactionMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionmultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMultithreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
